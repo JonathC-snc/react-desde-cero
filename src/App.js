ef34fb40ebdc1ad22af385f3dfee625d02091c5b
Jonathan <jonacris2007@gmail.com>
2021-04-06 18:42:30 -0400

    upadte del modulo 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
-import logo from './logo.svg';
 import "./styles/styles.scss";
 import Curso from './curso'
+
+const cursos = [
+  {
+    "titulo": "React desde cero",
+    "image": "https://edteam-media.s3.amazonaws.com/courses/small/f7dad9a6-e060-4305-9adf-b9a9079075de.jpg",
+    "price": 50,
+    "profesor": "Beto Quiroga"
+  },
+  {
+    "titulo": "Drupal desde cero",
+    "image": "https://edteam-media.s3.amazonaws.com/courses/small/5ec26692-8451-4297-a59a-adb1829a90e8.jpg",
+    "price": 40,
+    "profesor": "ramon snachez"
+  },
+  {
+    "titulo": "Go desde cero",
+    "image": "https://edteam-media.s3.amazonaws.com/courses/small/cd6b19fb-bfcb-4197-bf68-4fd2ab7f37e2.png",
+    "price": 30,
+    "profesor": "juan clavo"
+  },
+  {
+    "titulo": "Html desde cero",
+    "image": "https://edteam-media.s3.amazonaws.com/courses/small/26557907-0555-427e-a40c-6ff207f98d72.png",
+    "price": 20,
+    "profesor": "sergio boch"
+  }
+]
 
 const App = () => (
   <>
@@ -18,12 +44,9 @@ const App = () => (
   </div>
 
   <div className="ed-grid m-grid-3">
-    <Curso />
-    <Curso />
-    <Curso />
-    <Curso />
-    <Curso />
-    <Curso />
+    {
+      cursos.map( c => <Curso title={c.titulo} image={c.image} price={c.price} profesor={c.profesor}  />)
+    }
   </div>  
   </>
 )
--- src/curso.jsx
+++ src/curso.jsx
@@ -1,29 +1,37 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Curso = () => (
+const Curso = ({title, image, price, profesor}) => (
       <article className="card">
       <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-        <img src="https://edteam-media.s3.amazonaws.com/courses/small/2a257023-a927-4a13-947c-2efd87045e92.png" alt="curso chino"/>
+        <img src={image} alt={title}/>
       </div>
       <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
         <h3 className="t5 s-mb-2 s-center">
-          Programación orientada a objetos con Go
+          {title}
         </h3>
-        <div className="s-mb-2 s-main-center">
-          <div className="card__teacher s-cross-center">
-            <div className="card__avatar s-mr-1">
-              <div className="circle img-container">
-                <img src="https://edteam-media.s3.amazonaws.com/users/thumbnail/a3df2f81-3f56-474d-a002-58cb00bcf53d.png" alt="el chino"/>
-              </div>
-            </div>
-            <span className="small">Alexys Lozada</span>
-          </div>
+        <div className="s-main-center">
+          {`prof: ${profesor}`}
         </div>
         <div className="s-main-center">
-          <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
+          <a className="button--ghost-alert button--tiny" href="#">{`$ ${price}`}</a>
         </div>
       </div>
     </article>
 )
+
+Curso.propTypes = {
+  title: PropTypes.string,
+  image: PropTypes.string,
+  price: PropTypes.number,
+  profesor: PropTypes.string
+}
+
+Curso.defaultProps = {
+  title: "No se encontró titulo",
+  image: "http://www.ciudaddelapunta.com/sitio/fotos/ciudad/miniaturas/005.jpg",
+  price: "--",
+  profesor: ""
+}
 
 export default Curso
